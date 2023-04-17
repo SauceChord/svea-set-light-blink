6aa0bfda567f4fd068091ca91b1a9258e119992b
SauceChord <58859889+SauceChord@users.noreply.github.com>
2023-04-17 21:59:53 +0200

    Blinking lights

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,24 @@
--- Here's to the crazy ones. The misfits. The rebels. The troublemakers.
+-- Configuration
+local ShiftOffset = 0.000 --export: (Seconds) Base shift offset. Use this if chaining more than 10 lights.
+local DurationOn = 0.100 --export: (Seconds)
+local DurationOff = 0.900 --export: (Seconds)
 
--- The code below is just a "Hello World" that dumps the current links and their element classes
-system.print('Debugging Control Unit...')
-for linkName, element in pairs(library.getLinks()) do
-  system.print(string.format('Found link `%s` of type `%s`', linkName, element.getClass()))
+-- Script assumes all slots are either a light or nil
+local lights = { slot1, slot2, slot3, slot4, slot5, slot6, slot7, slot8, slot9, slot10 }
+local shift = ShiftOffset
+for i = 1, #lights do
+  local light = lights[i]
+  if light then
+    light.setOnBlinkingDuration(DurationOn)
+    light.setOffBlinkingDuration(DurationOff)
+    light.setBlinkingTimeShift(shift)
+    light.setBlinkingState(true)
+    light.activate()
+    shift = shift + DurationOn
+  end
 end
 
--- Stops execution
+-- Output shift duration if user need to chain more lights together
+system.print(string.format("Total shift duration: %f seconds", shift))
+
 unit.exit()
